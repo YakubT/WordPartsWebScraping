81baadaf20c53c2a4dd485d144b224bd6c58bd46
= <goldyakub@gmail.com>
2024-05-04 18:08:28 +0300

    implemented parsing

--- WordPartsWebScraping/src/main/java/com/yakub/Main.java
+++ WordPartsWebScraping/src/main/java/com/yakub/Main.java
@@ -1,7 +1,43 @@
 package com.yakub;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.yakub.pages.Gorokh;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String baseUrl = "https://goroh.pp.ua/Словозміна/";
+        String adjUrl = baseUrl + "[прикметник]";
+        String nounUrl = baseUrl + "[іменник]";
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        parseData(driver,adjUrl,"adj.txt");
+        parseData(driver,nounUrl,"noun.txt");
+    }
+
+    private static void parseData(WebDriver driver, String url, String path) {
+        Gorokh gorokh = new Gorokh(driver);
+        gorokh.setUrl(url);
+        gorokh.open();
+        do {
+            List<String> res = gorokh.getWordsOnPage();
+            try (FileWriter writer = new FileWriter(path, true)) {
+                for (String s : res) {
+                    writer.write(s + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } while (gorokh.nextPage());
     }
 }
---
+++ WordPartsWebScraping/src/main/java/com/yakub/pages/Gorokh.java
@@ -0,0 +1,48 @@
+package com.yakub.pages;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Gorokh {
+
+    private WebDriver driver;
+
+    private String url;
+
+    @FindBy(xpath = "//li[contains(@class,'content-list')]")
+    private List<WebElement> words;
+
+    @FindBy(xpath = "//a[contains(text(),'Наступна')]")
+    private WebElement nextButton;
+
+    public Gorokh(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void open() {
+        driver.get(url);
+    }
+
+    public List<String> getWordsOnPage() {
+        return words.stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    public boolean nextPage() {
+        if (!nextButton.isDisplayed()) {
+            return false;
+        }
+        nextButton.click();
+        return true;
+    }
+
+}
